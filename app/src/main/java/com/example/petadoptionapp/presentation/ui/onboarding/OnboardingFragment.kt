d6b98e4df72e94f0ad68ff9c2dd403b33f592cfd
Cristina Onica <cristina.onica@mobiversal.com>
2023-03-26 17:42:16 +0300

    started working on the onboarding screens

---
+++ app/src/main/java/com/example/petadoptionapp/presentation/ui/onboarding/OnboardingFragment.kt
@@ -0,0 +1,32 @@
+package com.example.petadoptionapp.presentation.ui.onboarding
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.petadoptionapp.R
+
+class OnboardingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OnboardingFragment()
+    }
+
+    private lateinit var viewModel: OnboardingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OnboardingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
