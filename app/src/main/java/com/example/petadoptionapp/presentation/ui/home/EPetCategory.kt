e60ede0eaf71602cd0268422bee4d3bdb1feeb5b
Cristina Onica <cristina.onica@mobiversal.com>
2023-05-07 23:34:26 +0300

    updated EPetCategory enum class

--- app/src/main/java/com/example/petadoptionapp/presentation/ui/home/EPetCategory.kt
+++ app/src/main/java/com/example/petadoptionapp/presentation/ui/home/EPetCategory.kt
@@ -7,5 +7,19 @@ enum class EPetCategory(val stringResource: Int, val iconResource: Int) {
     DOG(R.string.dog_pet_category, R.drawable.ic_dog_category_icon),
     CAT(R.string.cat_pet_category, R.drawable.ic_cat_category_icon),
     BIRD(R.string.bird_pet_category, R.drawable.ic_bird_category),
-    RABBIT(R.string.rabbit_pet_category, R.drawable.ic_rabbit_category)
+    RABBIT(R.string.rabbit_pet_category, R.drawable.ic_rabbit_category);
+
+    fun getPetCategoryString(): String {
+        return name.lowercase()
+    }
+
+    companion object {
+        fun getPetCategoryFromString(category: String): EPetCategory {
+            for (element in values()) {
+                if (element.name.lowercase() == category)
+                    return element
+            }
+            return ALL
+        }
+    }
 }
