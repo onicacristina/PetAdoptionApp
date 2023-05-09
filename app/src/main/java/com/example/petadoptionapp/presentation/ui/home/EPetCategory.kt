bef43e1bc51d0b1f1d4a0c4ba4ad0147b16eba56
Cristina Onica <cristina.onica@mobiversal.com>
2023-05-09 17:30:45 +0300

    some updates

--- app/src/main/java/com/example/petadoptionapp/presentation/ui/home/EPetCategory.kt
+++ app/src/main/java/com/example/petadoptionapp/presentation/ui/home/EPetCategory.kt
@@ -4,7 +4,7 @@ import com.example.petadoptionapp.R
 
 enum class EPetCategory(val stringResource: Int, val iconResource: Int) {
     ALL(R.string.all_pet_category, R.drawable.img_pets),
-    DOG(R.string.dog_pet_category, R.drawable.img_dog),
+    DOG(R.string.dog_pet_category, R.drawable.img_dog2),
     CAT(R.string.cat_pet_category, R.drawable.img_cat),
     BIRD(R.string.bird_pet_category, R.drawable.img_bird),
     RABBIT(R.string.rabbit_pet_category, R.drawable.img_rabbit);
