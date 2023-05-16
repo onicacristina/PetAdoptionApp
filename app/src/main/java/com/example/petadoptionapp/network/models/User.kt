ced2c0a105daaf6af50509d55535af3f0dfe48dc
Cristina Onica <cristina.onica@mobiversal.com>
2023-05-16 21:24:47 +0300

    updated user model

--- app/src/main/java/com/example/petadoptionapp/network/models/User.kt
+++ app/src/main/java/com/example/petadoptionapp/network/models/User.kt
@@ -1,8 +1,11 @@
 package com.example.petadoptionapp.network.models
 
 data class User(
-    val fullName: String,
+    val id: String,
     val role: Int = 0,
+    val firstName: String,
+    val lastName: String,
+    val phone: String,
     val email: String,
     val createdAt: String
 )
