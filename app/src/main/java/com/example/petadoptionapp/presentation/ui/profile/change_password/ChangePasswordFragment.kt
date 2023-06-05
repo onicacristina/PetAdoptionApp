92b69d3ac6663f96c51d6a9c4ac38058d4b04fe6
Cristina Onica <cristina.onica@mobiversal.com>
2023-06-06 01:55:25 +0300

    working on change password

--- app/src/main/java/com/example/petadoptionapp/presentation/ui/profile/change_password/ChangePasswordFragment.kt
+++ app/src/main/java/com/example/petadoptionapp/presentation/ui/profile/change_password/ChangePasswordFragment.kt
@@ -2,12 +2,20 @@ package com.example.petadoptionapp.presentation.ui.profile.change_password
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.petadoptionapp.R
 import com.example.petadoptionapp.databinding.FragmentChangePasswordBinding
 import com.example.petadoptionapp.presentation.base.NoBottomNavigationFragment
+import com.example.petadoptionapp.presentation.ui.authentication.InfoOrErrorAuthentication
+import com.example.petadoptionapp.presentation.utils.extensions.setOnDebounceClickListener
 import com.example.petadoptionapp.presentation.utils.extensions.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ChangePasswordFragment :
@@ -25,14 +33,65 @@ class ChangePasswordFragment :
     }
 
     private fun initViews() {
-
+        viewBinding.toolbar.tvTitle.text = getString(R.string.change_password)
     }
 
     private fun initListeners() {
+        viewBinding.toolbar.ivBack.setOnClickListener {
+            navController.popBackStack()
+        }
 
+        viewBinding.etCurrentPassword.doAfterTextChanged { text ->
+            viewModel.onPasswordChanged(text.toString())
+        }
+        viewBinding.etNewPassword.doAfterTextChanged { text ->
+            viewModel.onNewPasswordChanged(text.toString())
+        }
+        viewBinding.etConfirmNewPassword.doAfterTextChanged { text ->
+            viewModel.onConfirmNewPasswordChanged(text.toString())
+        }
+
+        viewBinding.btnSave.setOnDebounceClickListener {
+            //todo
+        }
     }
 
     private fun initObservers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                launch {
+                    viewModel.state.collect { value ->
+                        render(value)
+                    }
+                }
+                launch {
+                    viewModel.event.collect { value ->
+                        onEvent(value)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun onEvent(value: ChangePasswordViewModel.Event) {
+
+    }
+
+    private fun render(value: ChangePasswordViewModel.State) {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateSaveButton(isActive: Boolean) {
+        viewBinding.btnSave.isEnabled = isActive
+        viewBinding.btnSave.alpha = if (isActive) 1f else .6f
+    }
+
+    private fun showInfoOrError(infoOrError: InfoOrErrorAuthentication) {
+        viewBinding.clInfoOrError.isVisible = true
+        viewBinding.tvInfoOrError.text = infoOrError.stringResource?.let { getString(it) }
+    }
+
+    private fun hideInfoOrError() {
+        viewBinding.clInfoOrError.isVisible = false
     }
 }
--- app/src/main/java/com/example/petadoptionapp/presentation/ui/profile/change_password/ChangePasswordViewModel.kt
+++ app/src/main/java/com/example/petadoptionapp/presentation/ui/profile/change_password/ChangePasswordViewModel.kt
@@ -1,10 +1,48 @@
 package com.example.petadoptionapp.presentation.ui.profile.change_password
 
 import com.example.petadoptionapp.presentation.base.BaseViewModel
+import com.example.petadoptionapp.presentation.utils.DefaultEventDelegate
+import com.example.petadoptionapp.presentation.utils.DefaultStateDelegate
+import com.example.petadoptionapp.presentation.utils.EventDelegate
+import com.example.petadoptionapp.presentation.utils.StateDelegate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ChangePasswordViewModel @Inject constructor() : BaseViewModel() {
-    // TODO: Implement the ViewModel
+class ChangePasswordViewModel @Inject constructor(
+
+) : BaseViewModel(),
+    StateDelegate<ChangePasswordViewModel.State> by DefaultStateDelegate(State.default),
+    EventDelegate<ChangePasswordViewModel.Event> by DefaultEventDelegate() {
+
+    fun onPasswordChanged(data: String) {
+        currentState = currentState.copy(password = data)
+    }
+
+    fun onNewPasswordChanged(data: String) {
+        currentState = currentState.copy(newPassword = data)
+    }
+
+    fun onConfirmNewPasswordChanged(data: String) {
+        currentState = currentState.copy(confirmNewPassword = data)
+    }
+
+    data class State(
+        val password: String,
+        val newPassword: String,
+        val confirmNewPassword: String,
+        val isEnabledSaveButton: Boolean = false
+    ) {
+        companion object {
+            val default: State
+                get() = State("", "", "")
+        }
+    }
+
+    enum class Event {
+        SUCCESS,
+        SHORT_PASSWORD,
+        NO_MATCH_PASSWORD,
+        CHANGE_FAILURE
+    }
 }
