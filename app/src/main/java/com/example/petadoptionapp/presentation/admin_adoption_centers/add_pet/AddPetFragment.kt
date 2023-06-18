f660131e011325c350d05313466bbfd00a36fb0a
Cristina Onica <cristina.onica@mobiversal.com>
2023-06-18 17:21:19 +0300

    working on add pet

--- app/src/main/java/com/example/petadoptionapp/presentation/admin_adoption_centers/add_pet/AddPetFragment.kt
+++ app/src/main/java/com/example/petadoptionapp/presentation/admin_adoption_centers/add_pet/AddPetFragment.kt
@@ -1,6 +1,10 @@
 package com.example.petadoptionapp.presentation.admin_adoption_centers.add_pet
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
@@ -14,6 +18,9 @@ import com.example.petadoptionapp.presentation.utils.extensions.setOnDebounceCli
 import com.example.petadoptionapp.presentation.utils.extensions.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+
+
+const val PICK_IMAGE_REQUEST = 1 // Request code for selecting an image
 
 @AndroidEntryPoint
 class AddPetFragment :
@@ -65,6 +72,11 @@ class AddPetFragment :
         viewBinding.tvSpecie.setOnDebounceClickListener {
             navController.navigate(R.id.selectPetSpecieFragment)
         }
+
+        viewBinding.ivPetImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        }
     }
 
 
@@ -93,8 +105,7 @@ class AddPetFragment :
 
     private fun render(state: AddPetViewModel.State) {
         updateSaveButton(state.isEnabledButton)
-        if (viewModel.isNotEmptyGender())
-        {
+        if (viewModel.isNotEmptyGender()) {
             viewBinding.tvGender.text = getString(state.gender.stringResource)
         }
         if (viewModel.isNotEmptySpecie())
@@ -105,4 +116,18 @@ class AddPetFragment :
         viewBinding.btnSave.isEnabled = isActive
         viewBinding.btnSave.alpha = if (isActive) 1f else .6f
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+
+            // Perform further operations with the selected image URI, such as uploading to a server
+            // or displaying it in an ImageView.
+//            uploadImage(selectedImageUri)
+            viewBinding.ivPetImage.setImageURI(selectedImageUri)
+        }
+    }
+
 }
